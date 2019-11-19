a510ec05348f9f2e223ce50e776abb3ab6d4188e
Matheusmlx <matheusluiz0303@outlook.com>
2019-11-19 08:41:46 -0400

    operadores_de_atribuição

---
+++ Fundamentos/operadores_de_atribuição.py
@@ -0,0 +1,27 @@
+a = 3
+a = a+7
+print(a)
+
+a += 3  # == a = a +3
+print(a)
+
+a = 2
+a *= 2
+print(a)
+
+a /= 2
+print(a)
+
+a **= 8
+print(a)
+
+a //= 3.4  # simplifica o valor para um valor inteiro
+print(a)
+
+a /= 3.4
+print(a)
+
+
+a = 4
+a %= 3
+print(a)
