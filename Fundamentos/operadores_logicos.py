474913502cd7a196c2e6f7bc0bb66b5d23c98d7b
Matheusmlx <matheusluiz0303@outlook.com>
2019-11-19 08:52:06 -0400

    Operadores_logicos

---
+++ Fundamentos/operadores_logicos.py
@@ -0,0 +1,17 @@
+a = 3
+b = 5
+
+
+print(a == 3 and a == b)
+
+print(a == 3 and a != b)
+
+print(a == 3 or a == b)
+
+# Tabela verdade
+print('####Tabela Verdade#####')
+print(True and True)
+print(False and False)
+print(True and False)
+print(True or False)
+print(False or False)
